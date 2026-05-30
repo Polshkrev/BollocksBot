6e2d366a6c2e84415b4518ca124109720316f4e2
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-05-30 16:12:06 -0400

    Add irc client

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Polshkrev/BollocksBot
+
+go 1.25.0
+
+require (
+	github.com/Polshkrev/gopolutils v1.35.0
+	github.com/gorilla/websocket v1.5.3
+)
---
+++ models/irc/irc.go
@@ -0,0 +1,76 @@
+package irc
+
+import (
+	"github.com/Polshkrev/gopolutils"
+	"github.com/gorilla/websocket"
+)
+
+// An IRC client.
+type IRC struct {
+	connection *websocket.Conn
+}
+
+// Contruct a new IRC client with a given url. This constructor calls the private dial method.
+// Returns a new IRC client.
+func New(url string) *IRC {
+	var irc *IRC = new(IRC)
+	irc.dial(url)
+	return irc
+}
+
+// Dial a websocket with a given url.
+func (irc *IRC) dial(url string) {
+	var connection *websocket.Conn
+	var dialError error
+	connection, _, dialError = websocket.DefaultDialer.Dial(url, nil)
+	if dialError != nil {
+		panic(gopolutils.NewNamedException(gopolutils.RuntimeError, "%s\n", dialError.Error()))
+	}
+	irc.connection = connection
+}
+
+// Write a given message to the irc connection.
+// If the message can not be written, a [gopolutils.RuntimeError] is returned.
+func (irc *IRC) Write(message string) *gopolutils.Exception {
+	var except error = irc.connection.WriteMessage(websocket.TextMessage, []byte(message))
+	if except != nil {
+		return gopolutils.NewNamedException(gopolutils.RuntimeError, "%s\n", except.Error())
+	}
+	return nil
+}
+
+// Concurrently read from a given websocket connection.
+func readConcurrent(connection *websocket.Conn, resultChannel chan<- []byte, errorChannel chan<- error) {
+	defer close(resultChannel)
+	defer close(errorChannel)
+	var result []byte
+	var readError error
+	_, result, readError = connection.ReadMessage()
+	resultChannel <- result
+	errorChannel <- readError
+}
+
+// Read from the irc connection.
+// Returns the string representation of the irc connection.
+// If the connection can not be read, a [gopolutils.RuntimeError] is returned with an empty string.
+func (irc *IRC) Read() (string, *gopolutils.Exception) {
+	var resultChannel chan []byte = make(chan []byte, 1)
+	var errorChannel chan error = make(chan error, 1)
+	go readConcurrent(irc.connection, resultChannel, errorChannel)
+	var result []byte = <-resultChannel
+	var except error = <-errorChannel
+	if except != nil {
+		return "", gopolutils.NewNamedException(gopolutils.RuntimeError, "%s\n", except.Error())
+	}
+	return string(result), nil
+}
+
+// Close the irc connection.
+// If closing the connection fails, a [gopolutils.RuntimeError] is returned.
+func (irc *IRC) Close() *gopolutils.Exception {
+	var closeError error = irc.connection.Close()
+	if closeError != nil {
+		return gopolutils.NewNamedException(gopolutils.RuntimeError, "%s\n", closeError.Error())
+	}
+	return nil
+}
